b0a8e689b590a9adf7aba7219568b9f5c49baba6
reszz <salirarestu@gmail.com>
2026-04-17 15:30:09 +0700

    changes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\MahasiswaController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/halo', function (){
+    return 'Halo, Salira Selamat Datang di Praktikum Laravel';
+});
+
+Route::get('/user/{id}', function ($id){
+    return "Profile User dengan ID: ". $id;
+});
+
+Route::get('/produk/{slug?}', function($slug = 'semua-produk') {
+    return "Menampilkan: ". $slug;
+});
+
+Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
